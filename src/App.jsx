d1fd9f50a8c7a09a8b9cbb93ef91f3c08553c69f
Hemerson Daniel <adv.hemersondaniel@hotmail.com>
2025-05-25 21:50:59 -0300

    fix: corrige import case-sensitive de Perfil em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// src/App.jsx
+k// src/App.jsx
 import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Navbar from './Navbar';
@@ -9,7 +9,7 @@ import Auth from './pages/Auth';
 import Sobre from './pages/Sobre';
 import Contato from './pages/Contato';
 import Dashboard from './pages/Dashboard';
-import Perfil from './pages/Dashboard/perfil';      // <— pasta "Dashboard" (D maiúsculo) e arquivo "perfil.jsx" (minúsculo)
+import Perfil from './pages/dashboard/perfil';      // pasta "dashboard" tudo minúsculo
 import CoursePlayer from './pages/CoursePlayer';
 import AdminLayout from './pages/AdminLayout';
 import DashboardLayout from './layouts/DashboardLayout';
